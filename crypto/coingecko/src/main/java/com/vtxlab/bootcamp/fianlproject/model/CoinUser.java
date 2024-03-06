0e565ebea04dca7a2a7bd02d5a7fdbdb4d0d97e6
Edwin Sung <edwardwong503@gmail.com>
2024-03-06 17:53:41 +0800

    update

--- crypto/coingecko/src/main/java/com/vtxlab/bootcamp/fianlproject/model/CoinUser.java
+++
@@ -1,9 +0,0 @@
-package com.vtxlab.bootcamp.fianlproject.model;
-
-import lombok.Getter;
-
-@Getter
-public class CoinUser {
-  
-  private String name;
-}
--- crypto/coingecko/src/main/java/com/vtxlab/bootcamp/fianlproject/service/RedisCoingeckoService.java
+++
@@ -1,9 +0,0 @@
-package com.vtxlab.bootcamp.fianlproject.service;
-
-import java.util.List;
-import com.vtxlab.bootcamp.fianlproject.dto.response.CoingeckoFinalDTO;
-
-public interface RedisCoingeckoService {
-  
-  CoingeckoFinalDTO  getCoinMarketData(String currency, List<String> coinId);
-}
